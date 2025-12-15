f7a3864e08aa955d932754c937166cd3c234ccca
Vst-Byte <matheusgvisentim@gmail.com>
2025-12-15 16:36:43 +0000

    first commit

---
+++ script_servidor_web.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+echo"Atualizando o Servidor"
+
+apt-get update
+apt-get upgrade -y
+apt-get install apache2 -y
+apt-get install unzip -y
+
+echo "Baixando e Copiando os Arquivos das Aplicacoes"
+
+wget https://github.com/Vst-Byte/Projeto-Site-Pessoal/archive/refs/heads/main.zip
+unzip main.zip
+cd Projeto-Site-Pessoal-main
+cp -R * /var/www/html/
